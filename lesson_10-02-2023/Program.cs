ff090a658dd0bdccc3ddea7683b8c3fb7bcf9bbd
Иван Котельников <ivankotelnikov@MacBook-Air-Ivan.local>
2023-02-11 02:18:25 +0300

    добавили еще одну задачу

--- lesson_10-02-2023/Program.cs
+++ lesson_10-02-2023/Program.cs
@@ -1,6 +1,24 @@
-﻿Console.Clear();
+﻿//Console.Clear();
+//Console.Write("Введите любое число: ");
+//double number = Convert.ToDouble(Console.ReadLine());
+//double result = number * number;
+//Console.WriteLine("Квадрат от числа: " + number + " равен " + result);
 
-Console.Write("Введите любое число: ");
-double number = Convert.ToDouble(Console.ReadLine());
-double result = number * number;
-Console.WriteLine("Квадрат от числа: " + number + " равен " + result);
+
+
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int firstValue = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondValue = Convert.ToInt32(Console.ReadLine());
+
+if (firstValue == Math.Pow(secondValue, 2))
+{
+    Console.WriteLine("Число " + firstValue + " является квадратом от " + secondValue);
+}
+else
+{
+    Console.WriteLine("Число " + firstValue + " не является квадратом от " + secondValue);
+}
