6e9421f5d94f28a1ddc9e2a8af0c156df797fccd
Иван Котельников <ivankotelnikov@MacBook-Air-Ivan.local>
2023-02-11 14:55:01 +0300

    Add task 4 completed

--- HomeWork_Task_4/Task_4.cs
+++ HomeWork_Task_4/Task_4.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.Write("Введите первое число: ");
+double firstValue = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+double secondValue = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+double thirdValue = Convert.ToDouble(Console.ReadLine());
+
+double max = firstValue;
+
+if (firstValue > max) max = firstValue;
+if (secondValue > max) max = secondValue;
+if (thirdValue > max) max = thirdValue;
+
+Console.WriteLine("Наибольшее число будет: " + max);
+
