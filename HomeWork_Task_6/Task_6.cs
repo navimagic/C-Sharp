9e2385368b3f64371616ad4dc3146e58aefd3e13
Иван Котельников <ivankotelnikov@MacBook-Air-Ivan.local>
2023-02-11 15:38:44 +0300

    Add Task 6 completed

---
+++ HomeWork_Task_6/Task_6.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.Write("Введите любое число: ");
+double number = Convert.ToDouble(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Введенное вами число является четным")
+;}
+if (number % 2 == 1)
+{
+    Console.WriteLine("Введенное вами число НЕ является четным");
+}
