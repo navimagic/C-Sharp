aeb0e41120b6a0ad007eac30b6c1c25f97510516
Иван Котельников <ivankotelnikov@MacBook-Air-Ivan.local>
2023-02-11 19:33:19 +0300

    Add 8 Task Completed

---
+++ HomeWork_Task_8/Task_8.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+Console.Write("Введите любое число: ");
+double N = Convert.ToDouble(Console.ReadLine());
+double count = 0;
+while (N > count)
+{
+    count = count + 2;
+    Console.WriteLine(count);
+}
