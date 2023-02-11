c44cf8f31c9036b4f218dcef5b04576259db1d23
Иван Котельников <ivankotelnikov@MacBook-Air-Ivan.local>
2023-02-11 14:25:38 +0300

    add task 2 completed

---
+++ HomeWork/Task 2.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+double firstValue = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double secondValue = Convert.ToDouble(Console.ReadLine());
+if (firstValue > secondValue)
+{
+    Console.WriteLine("Число " + firstValue + " больше чем " + secondValue);
+}
+else
+{
+    Console.WriteLine("Число " + secondValue + " больше чем " + firstValue);
+}
